61171b8f12099f5c3738dc322d66f81cfdd40c4c
Haohero37 <tabahao01628905972@gmail.com>
2023-12-31 01:17:18 +0700

    Thêm lệnh in Xin Chào

--- TESTPROJECT/src/testproject/TESTPROJECT.java
+++ TESTPROJECT/src/testproject/TESTPROJECT.java
@@ -14,7 +14,7 @@ public class TESTPROJECT {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Xin chào ");
     }
     
 }
